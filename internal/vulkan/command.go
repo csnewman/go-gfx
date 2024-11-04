f2e1c298480fac091af3116812697e651093d60d
Chandler Newman <chandler2newman@hotmail.co.uk>
2024-11-04 13:32:50 +0000

    Add basic rendering with vk

--- internal/vulkan/command.go
+++ internal/vulkan/command.go
@@ -45,6 +45,23 @@ VkResult gfx_vkQueueSubmit2KHR(
 	return GFX_VK_EXT_FUNC(vkQueueSubmit2KHR, queue, submitCount, pSubmits, fence);
 }
 
+void gfx_vkCmdSetViewportWithCountEXT(
+    VkInstance                                  instance,
+	VkCommandBuffer                             commandBuffer,
+    uint32_t                                    viewportCount,
+    const VkViewport*                           pViewports
+) {
+	GFX_VK_EXT_FUNC(vkCmdSetViewportWithCountEXT, commandBuffer, viewportCount, pViewports);
+}
+
+void gfx_vkCmdSetScissorWithCountEXT(
+    VkInstance                                  instance,
+	VkCommandBuffer                             commandBuffer,
+    uint32_t                                    scissorCount,
+    const VkRect2D*                             pScissors
+) {
+	GFX_VK_EXT_FUNC(vkCmdSetScissorWithCountEXT, commandBuffer, scissorCount, pScissors);
+}
 */
 import "C"
 
@@ -134,11 +151,33 @@ func (c *CommandBuffer) BeginRenderPass(description hal.RenderPassDescriptor) {
 	pinner.Pin(renderingInfo.pColorAttachments)
 
 	C.gfx_vkCmdBeginRenderingKHR(c.graphics.instance, c.commandBuffer, &renderingInfo)
+
+	var viewport C.VkViewport
+	viewport.x = C.float(0)
+	viewport.y = C.float(0)
+	viewport.width = C.float(c.frame.img.width)
+	viewport.height = C.float(c.frame.img.height)
+	viewport.minDepth = C.float(0)
+	viewport.maxDepth = C.float(1)
+
+	C.gfx_vkCmdSetViewportWithCountEXT(c.graphics.instance, c.commandBuffer, 1, &viewport)
+
+	var scissor C.VkRect2D
+	scissor.offset.x = 0
+	scissor.offset.y = 0
+	scissor.extent.width = C.uint32_t(c.frame.img.width)
+	scissor.extent.height = C.uint32_t(c.frame.img.height)
+
+	C.gfx_vkCmdSetScissorWithCountEXT(c.graphics.instance, c.commandBuffer, 1, &scissor)
 }
 
 func (c *CommandBuffer) SetRenderPipeline(pipeline hal.RenderPipeline) {
-	//TODO implement me
-	panic("implement me")
+	p, ok := pipeline.(*RenderPipeline)
+	if !ok {
+		panic("unexpected type")
+	}
+
+	C.vkCmdBindPipeline(c.commandBuffer, C.VK_PIPELINE_BIND_POINT_GRAPHICS, p.pipeline)
 }
 
 func (c *CommandBuffer) SetVertexBuffer(data hal.Buffer) {
@@ -147,8 +186,7 @@ func (c *CommandBuffer) SetVertexBuffer(data hal.Buffer) {
 }
 
 func (c *CommandBuffer) Draw(start int, count int) {
-	//TODO implement me
-	panic("implement me")
+	C.vkCmdDraw(c.commandBuffer, C.uint32_t(count), 1, C.uint32_t(start), 0)
 }
 
 func (c *CommandBuffer) EndRenderPass() {
